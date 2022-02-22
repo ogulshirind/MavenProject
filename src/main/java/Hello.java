3f2f816dd4a4e89348a68c6b9e6b3fa3132280c4
ogulshirind <ogulshirin.dosyyeva@gmail.com>
2022-02-23 03:55:03 +0700

    update added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing hello feature");
+        System.out.println("local repo is already published to github");
+        System.out.println("doing more work");
     }
 }
